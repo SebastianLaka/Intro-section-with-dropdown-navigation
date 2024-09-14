c691ff05b17a96a70d581540129379a95e0c09b8
Sebastian <sebastianlakla@gmail.com>
2024-09-14 16:02:04 +0200

    Add fluent slide for nav menu at mobile and fixed nav

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 "use strict";
 const burgerIcon = document.querySelector(".nav__hamburger-icon");
-const closeNavIcon = document.querySelector('.nav-container__close-icon')
-const navContainer = document.querySelector('.nav-container')
-
-burgerIcon.addEventListener('click', e =>{
-    if(e.target.tagName === "BUTTON"){
-        navContainer.classList.add('show-slider');
-    }
-})
-closeNavIcon.addEventListener('click', ()=>{
-    navContainer.classList.remove('show-slider');
-})
+const closeNavIcon = document.querySelector(".nav-container__close-icon");
+const navContainer = document.querySelector(".nav-container");
+const body = document.body;
+burgerIcon.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    navContainer.classList.remove("hide-slider");
+    navContainer.classList.add("show-slider");
+  }
+});
+closeNavIcon.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    navContainer.classList.remove("show-slider"); 
+    navContainer.classList.add("hide-slider"); 
+  }
+});
