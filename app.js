155362b0beda55509fda02be042b97a81a568391
Sebastian <sebastianlakla@gmail.com>
2024-09-14 15:19:40 +0200

    Change hamburger icon structure in html and css. Add event delegation at burger icon

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 "use strict";
 const burgerIcon = document.querySelector(".nav__hamburger-icon");
 const closeNavIcon = document.querySelector('.nav-container__close-icon')
+const navContainer = document.querySelector('.nav-container')
 
-
+burgerIcon.addEventListener('click', e =>{
+    if(e.target.tagName === "BUTTON"){
+        navContainer.classList.add('show-slider');
+    }
+})
